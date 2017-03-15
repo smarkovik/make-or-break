c11451dac97ad4c383a6a37dfcd6bd39d0fb162a
Tancho <tancho@contentsquare.com>
2017-03-15 16:29:54 +0100

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/SampleSDK.java
@@ -0,0 +1,13 @@
+package com.example.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by tancho on 3/15/17.
+ */
+
+public class SampleSDK {
+    public static final void runSdk() {
+        Log.e("SDK", "SDK Started");
+    }
+}
